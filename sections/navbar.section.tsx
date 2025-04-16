e2f42ba6318ce958c722038ec82d8df924e59143
optimatech765 <optimatech88@gmail.com>
2025-04-16 08:25:19 +0200

    correction de la responsivité

--- sections/navbar.section.tsx
+++ sections/navbar.section.tsx
@@ -18,42 +18,46 @@ export const NavbarSection = () => {
             position={"sticky"}
             isBordered={true}>
 
+
             {/* Menu burger (affiché sur mobile) */}
             <Button isIconOnly variant="light" className="lg:hidden text-primary" onPress={() => setIsOpen(true)}>
                 <Menu size={24} className="text-primary" fill="#ee3540" />
             </Button>
 
+
             {/* Logo */}
             <NavbarBrand>
-                <Image height={40} width={40} src="/img/logo.png" alt="Logo" className="h-16 w-auto" />
+                <div className="flex items-center justify-end lg:justify-between gap-2 flex-1">
+                    <Image height={40} width={40} src="/img/logo.png" alt="Logo" className="h-16 w-auto" />
+                </div>
             </NavbarBrand>
 
-            <NavbarContent className="lg:hidden">
-                <Drawer isOpen={isOpen} onOpenChange={setIsOpen} placement="left">
-                    <DrawerContent className="w-64 p-4">
-                        <nav className="flex flex-col gap-4">
-                            <LinkH as={Link} href="/" onPress={() => setIsOpen(false)} className="text-lg font-semibold text-black">
-                                Accueil
-                            </LinkH>
-                            <LinkH as={Link} href="#evenements" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
-                                Événements
-                            </LinkH>
-                            <LinkH as={Link} href="#categories" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
-                                Catégories
-                            </LinkH>
-                            <LinkH as={Link} href="#fonctionnalites" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
-                                Fonctionnalités
-                            </LinkH>
-                            <LinkH as={Link} href="/login" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
-                                Se connecter
-                            </LinkH>
-                            <Button as={Link} href="/register" className="font-bold mt-4 bg-primary text-white">
-                                S’inscrire
-                            </Button>
-                        </nav>
-                    </DrawerContent>
-                </Drawer>
-            </NavbarContent>
+
+            <Drawer className="lg:hidden" isOpen={isOpen} onOpenChange={setIsOpen} placement="left">
+                <DrawerContent className="w-64 p-4">
+                    <nav className="flex flex-col gap-4">
+                        <LinkH as={Link} href="/" onPress={() => setIsOpen(false)} className="text-lg font-semibold text-black">
+                            Accueil
+                        </LinkH>
+                        <LinkH as={Link} href="#evenements" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
+                            Événements
+                        </LinkH>
+                        <LinkH as={Link} href="#categories" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
+                            Catégories
+                        </LinkH>
+                        <LinkH as={Link} href="#fonctionnalites" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
+                            Fonctionnalités
+                        </LinkH>
+                        <LinkH as={Link} href="/login" onPress={() => setIsOpen(false)} className="text-lg font-semibold">
+                            Se connecter
+                        </LinkH>
+                        <Button as={Link} href="/register" className="font-bold mt-4 bg-primary text-white">
+                            S’inscrire
+                        </Button>
+                    </nav>
+                </DrawerContent>
+            </Drawer>
+
 
 
             {/* Liens de navigation (cachés sur mobile) */}
