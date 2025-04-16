e7fb5416ec5243b0483b3da4ce4a6df6cfe4f86a
optimatech765 <optimatech88@gmail.com>
2025-04-16 07:49:29 +0200

    responsivité

--- components/myevent.card.component.tsx
+++ components/myevent.card.component.tsx
@@ -7,7 +7,7 @@ import { Plus } from 'lucide-react';
 export const MyEventEventCardComponent = () => {
     return (
         <Card>
-            <CardHeader className='flex justify-between items-center'>
+            <CardHeader className='flex flex-col md:flex-row justify-between items-center'>
                 <div>
                     <h3 className="text-lg font-bold text-primary">
                         Mes évènements
@@ -16,7 +16,7 @@ export const MyEventEventCardComponent = () => {
                         Evènements organisés
                     </p>
                 </div>
-                <Button startContent={<Plus className="w-4 h-4 text-white" />} size='sm' radius='full' color='primary' className="mt-2 border-1 text-xs font-light text-white">
+                <Button as={Link} href={"/user/events/new"} startContent={<Plus className="w-4 h-4 text-white" />} size='sm' radius='full' color='primary' className="mt-2 border-1 text-xs font-light text-white">
                     Créer un évènement
                 </Button>
                 <div>
