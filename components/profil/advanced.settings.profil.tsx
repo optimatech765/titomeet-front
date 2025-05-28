ae454c57bea84ef6075c1012cf487d0689d31ec7
optimatech765 <optimatech88@gmail.com>
2025-05-28 17:43:36 +0200

    Gestion du système de langue

--- components/profil/advanced.settings.profil.tsx
+++ components/profil/advanced.settings.profil.tsx
@@ -1,44 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from '@heroui/button';
 import { Radio, RadioGroup, useDisclosure } from '@heroui/react';
 import React from 'react';
 import { DesactiveAccountModal } from './desactive.account.modal';
 import { DeleteAccountModal } from './delete.account.modal';
+import { useChangeLocale, useCurrentLocale, useScopedI18n } from '@/locales/client';
 
 export const AdvancedSettingsProfil = () => {
-    const [selected, setSelected] = React.useState("london");
     const { isOpen, onOpen, onClose } = useDisclosure();
     const { isOpen: isOpenDesactive, onOpen: onOpenDesactive, onClose: onCloseDesactive } = useDisclosure();
+    const updateProfilT = useScopedI18n("updateProfil");
+    const locale = useCurrentLocale();
+    const changelOcale = useChangeLocale();
+
     return (
         <>
             <div className='flex flex-col justify-between h-full'>
                 <div>
                     <h2 className="text-2xl font-semibold mb-4">
-                        Paramètres avancés
+                        {updateProfilT("advancedTitle")}
                     </h2>
 
-                    <h4 className='text-sm font-medium text-gray-700'>Langue</h4>
+                    <h4 className='text-sm font-medium text-gray-700'>{updateProfilT("advancedLan")}</h4>
                     <div className="flex flex-col gap-3 mt-3">
-                        <RadioGroup value={selected} onValueChange={setSelected} className={"justify-start space-y-4"} >
+                        <RadioGroup value={locale} onValueChange={(e) => changelOcale(e as any)} className={"justify-start space-y-4"} >
 
                             <Radio value="fr"
-                                color='success'
+                                onChange={() => changelOcale("fr")}
+                                checked={locale === "fr"}
+                                color='danger'
                                 classNames={{
                                     label: "text-sm pl-0 font-medium text-gray-700 flex flex-row-reverse gap-2",
                                     description: "text-sm font-medium text-gray-700 flex flex-row-reverse gap-2",
                                     base: "flex flex-row-reverse gap-2 pl-0  my-1",
                                 }} >
-                                Français
+                                {updateProfilT("advancedFr")}
                             </Radio>
-
                             <Radio value="en"
-                                color='success'
+                                onChange={() => changelOcale("en")}
+                                checked={locale === "en"}
+                                color='danger'
                                 classNames={{
                                     label: "text-sm font-medium text-gray-700 flex flex-row-reverse gap-2",
                                     description: "text-sm font-medium text-gray-700 flex flex-row-reverse gap-2",
                                     base: "flex flex-row-reverse gap-2 pl-0",
                                 }}
                             >
-                                Anglais
+                                {updateProfilT("advancedEn")}
                             </Radio>
 
                         </RadioGroup>
@@ -49,10 +57,10 @@ export const AdvancedSettingsProfil = () => {
                         <h4 className='text-sm font-medium text-gray-700'>Compte</h4>
                         <div className="flex flex-col gap-3 mt-4 md:w-1/3">
                             <Button onPress={() => onOpenDesactive()} className="bg-tertiary font-semibold text-primary px-6 py-2  " radius='full' >
-                                Desactiver mon compte
+                                {updateProfilT("deactive")}
                             </Button>
                             <Button onPress={() => onOpen()} className="bg-tertiary font-semibold text-primary px-6 py-2  " radius='full' >
-                                Supprimer mon compte
+                                {updateProfilT("delete")}
                             </Button>
                         </div>
                     </div>
